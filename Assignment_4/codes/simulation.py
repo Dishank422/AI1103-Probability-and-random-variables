580d12c5cd84319d02f2bb10d04fc0a4fc0f1cf6
Dishank422 <75781936+Dishank422@users.noreply.github.com>
2021-03-23 11:43:41 +0530

    Create simulation.py

---
+++ Assignment_4/codes/simulation.py
@@ -0,0 +1,16 @@
+from scipy.stats import norm
+
+sim_len = 100000
+count = 0
+
+for i in range(sim_len):
+    X = 0
+    while 1:
+        X = norm.rvs()
+        if (-1 <= X < 0) or (1 <= X < 2):
+            break
+    if X < 0:
+        count += 1
+
+print("Experimental probability = ", count / sim_len)
+print("which is approximately equal to 0.716(theoritical probability)")
